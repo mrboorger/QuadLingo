d6e83230b9dd019c09f5e79b6e49e83f907f15bc
mrboorger <lolov.91@bk.ru>
2022-03-14 17:43:50 +0300

    Added music directory and music

--- music_class.cpp
+++ music_class.cpp
@@ -5,31 +5,44 @@ MusicClass::MusicClass(QWidget* parent)
       media_player_(new QMediaPlayer(this)),
       media_playlist_(new QMediaPlaylist(this)) {
   media_playlist_->setPlaybackMode(QMediaPlaylist::Loop);
-//  media_playlist_->addMedia(QUrl::fromLocalFile(
-//      "c:\\Users\\mrboorger\\CLionProjects\\QuadLingo\\cmake-build-debug\\music\\music.mp3"));
-//  media_playlist_->addMedia(QUrl::fromLocalFile(
-//      "c:\\Users\\mrboorger\\CLionProjects\\QuadLingo\\cmake-build-debug\\music\\music0.mp3"));
-//  media_playlist_->addMedia(QUrl::fromLocalFile(
-//      "c:\\Users\\mrboorger\\CLionProjects\\QuadLingo\\cmake-build-debug\\music\\music2.mp3"));
-//  media_playlist_->addMedia(QUrl::fromLocalFile(
-//      "c:\\Users\\mrboorger\\CLionProjects\\QuadLingo\\cmake-build-debug\\music\\music3.mp3"));
-//  media_playlist_->addMedia(QUrl::fromLocalFile(
-//      "c:\\Users\\mrboorger\\CLionProjects\\QuadLingo\\cmake-build-debug\\music\\.mp3"));
+  QDir directory;
+
+  try {
+    directory = GetMusicDir();
+  } catch (std::runtime_error& e) {
+    qDebug() << e.what() << '\n';
+    return;
+  }
+
+  QStringList music_list = directory.entryList(QStringList() << "*.mp3"
+                                                             << "*.MP3",
+                                               QDir::Files);
+
+  for (const auto& music_name : music_list) {
+    media_playlist_->addMedia(QUrl::fromLocalFile(directory.absolutePath() + "/" + music_name));
+  }
 
   media_player_->setPlaylist(media_playlist_);
   media_player_->setMedia(media_playlist_);
 }
 
-QMediaPlayer* MusicClass::GetPlayer() {
-  return media_player_;
-}
+QMediaPlayer* MusicClass::GetPlayer() { return media_player_; }
 
-QMediaPlaylist* MusicClass::GetPlaylist() {
-  return media_playlist_;
-}
+QMediaPlaylist* MusicClass::GetPlaylist() { return media_playlist_; }
 
 void MusicClass::CreateDialog() {
   auto* dialog(new SettingsDialog(this));
   dialog->exec();
 }
 
+bool MusicClass::IsMusicDirExists() const {
+  const QFileInfo music_dir_info(kMusicDirPath);
+  return music_dir_info.exists() && music_dir_info.isDir();
+}
+
+QDir MusicClass::GetMusicDir() {
+  if (!IsMusicDirExists()) {
+    throw std::runtime_error("Directory \"" + kMusicDirPath.toStdString() + "\"" + " doesn't exist\n");
+  }
+  return QDir(kMusicDirPath);
+}
--- music_class.h
+++ music_class.h
@@ -1,12 +1,13 @@
 #ifndef MUSIC_CLASS_H_
 #define MUSIC_CLASS_H_
 
-#include <QWidget>
-#include <QMultimedia>
-#include <QVBoxLayout>
-#include <QPushButton>
+#include <QDir>
 #include <QMediaPlayer>
 #include <QMediaPlaylist>
+#include <QMultimedia>
+#include <QPushButton>
+#include <QVBoxLayout>
+#include <QWidget>
 
 #include "settings_dialog.h"
 
@@ -19,11 +20,15 @@ class MusicClass : public QWidget {
   QMediaPlaylist* GetPlaylist();
 
   void CreateDialog();
- private:
-  QMediaPlayer* media_player_;
 
+ private:
+  inline static const QString kMusicDirPath = "./resources/music";
+
+  QMediaPlayer* media_player_;
   QMediaPlaylist* media_playlist_;
 
+  QDir GetMusicDir();
+  bool IsMusicDirExists() const;
 };
 
 #endif  // MUSIC_CLASS_H_
